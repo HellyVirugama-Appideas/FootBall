8715fda0adbf2fd4ecdb72af66bac41f14365361
tai-meetshah <theappideas.meet@gmail.com>
2024-02-07 11:55:57 +0530

    Changes in signup

--- routes/api/authRoutes.js
+++ routes/api/authRoutes.js
@@ -6,7 +6,7 @@ const authController = require('../../controllers/api/authController');
 
 router.post('/register', uploadResume, authController.register);
 
-router.post('/signup', uploadResume, authController.signup);
+router.post('/signup', authController.signup);
 
 router.post('/login', fileUpload(), authController.login);
 
